22fd1c2203d100ac23b8affe02c37979cd6872aa
46goh <4690goh@gmail.com>
2022-08-19 16:51:47 +0900

    let storybook support chakra-ui

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 module.exports = {
   stories: ["./src/**/*.story.@(js|jsx|ts|tsx)"],
   addons: [
+    "@chakra-ui/storybook-addon",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
+  staticDirs: ["../public"],
   core: {
     builder: "@storybook/builder-webpack5",
   },
